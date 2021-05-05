608cc1320a78324e1070ba98f2aa3c1c9c644ea8
davehamptonusa <david.hampton@gmail.com>
2021-05-05 07:34:23 -0700

    Trying to remove race condition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useEffect } from "react";
 import './App.css';
-import Whole from './components/Whole/Whole.lazy.js'
+import Whole from './components/Whole/Whole'
+import AOS from 'aos';
+import './aos.css';
 
 function App() {
+  useEffect(() => {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+      });
+  });
   return (
     <div className="App">
       <Whole/>
